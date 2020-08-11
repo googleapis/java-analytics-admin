7de207b38b48e8456c972416df9b718df806cf1c
Ilya Kuleshov <ikuleshov@google.com>
2020-08-11 14:01:54 -0700

    docs(samples): sample updated to match the style guide

--- samples/snippets/src/main/java/com/example/analyticsadmin/QuickstartSample.java
+++ samples/snippets/src/main/java/com/example/analyticsadmin/QuickstartSample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google Inc.
+ * Copyright 2020 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.analytics;
 
 /* This application demonstrates the usage of the Analytics Admin API using
@@ -46,7 +47,7 @@ package com.example.analytics;
    mvn exec:java -Dexec.mainClass="com.example.analytics.QuickstartSample"
  */
 
-// [START analytics-admin_quickstart]
+// [START analytics_admin_quickstart]
 
 import com.google.analytics.admin.v1alpha.Account;
 import com.google.analytics.admin.v1alpha.AnalyticsAdminServiceClient;
@@ -57,7 +58,16 @@ import com.google.analytics.admin.v1alpha.ListAccountsRequest;
 public class QuickstartSample {
 
   public static void main(String... args) throws Exception {
+    listAccounts();
+  }
+
+
+  // This is an example snippet that calls the Google Analytics Admin API and lists all Google
+  // Analytics accounts available to the authenticated user.
+  static void listAccounts() throws Exception {
     // Instantiates a client using default credentials.
+    // See https://cloud.google.com/docs/authentication/production for more information
+    // about managing credentials.
     try (AnalyticsAdminServiceClient analyticsAdmin = AnalyticsAdminServiceClient.create()) {
       // Calls listAccounts() method of the Google Analytics Admin API and prints
       // the response for each account.
@@ -65,11 +75,11 @@ public class QuickstartSample {
           .listAccounts(ListAccountsRequest.newBuilder().build());
       for (ListAccountsPage page : response.iteratePages()) {
         for (Account account : page.iterateAll()) {
-          System.out.printf("Account name: %s\n", account.getName());
-          System.out.printf("Display name: %s\n", account.getDisplayName());
-          System.out.printf("Country code: %s\n", account.getCountryCode());
-          System.out.printf("Create time: %s\n", account.getCreateTime().getSeconds());
-          System.out.printf("Update time: %s\n", account.getUpdateTime().getSeconds());
+          System.out.printf("Account name: %s%n", account.getName());
+          System.out.printf("Display name: %s%n", account.getDisplayName());
+          System.out.printf("Country code: %s%n", account.getCountryCode());
+          System.out.printf("Create time: %s%n", account.getCreateTime().getSeconds());
+          System.out.printf("Update time: %s%n", account.getUpdateTime().getSeconds());
           System.out.println();
         }
       }
@@ -77,4 +87,4 @@ public class QuickstartSample {
   }
 }
 
-// [END analytics-admin_quickstart]
+// [END analytics_admin_quickstart]
